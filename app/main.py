6f3a994e4ea70a5118efa279286a3ec892f8d8bf
Danil0Ribeir0 <danilo.ribeiro18rs@gmail.com>
2026-04-28 14:20:37 -0300

    feat: add cors para frontend

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router as projeto_router
 
 app = FastAPI(
     title="Lighthouse Academic API",
     description="Motor stateless de análise de saúde para repositórios acadêmicos.",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "*", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(projeto_router)
